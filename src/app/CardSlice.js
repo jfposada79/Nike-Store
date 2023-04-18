1543406a806d8975bac6e3746fceb3fb321252c0
jfposada79 <fernandoposada1@hotmail.com>
2023-04-18 12:37:16 -0500

    toaster and read from localstorage done

--- src/app/CardSlice.js
+++ src/app/CardSlice.js
@@ -1,8 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit"
+import { toast } from "react-hot-toast"
 
 const initialState = {
   cartState: false,
-  cartItems: []
+  cartItems: localStorage.getItem("cart")
+    ? JSON.parse(localStorage.getItem("cart"))
+    : []
 }
 
 const CartSlice = createSlice({
@@ -21,10 +24,14 @@ const CartSlice = createSlice({
       )
       if (itemIndex >= 0) {
         state.cartItems[itemIndex].cartQuantity += 1
+        toast.success(`Item QTY Increased`)
       } else {
         const temp = { ...action.payload, cartQuantity: 1 }
         state.cartItems.push(temp)
+        toast.success(`${action.payload.title} added to Cart`)
       }
+
+      localStorage.setItem("cart", JSON.stringify(state.cartItems))
     }
   }
 })
